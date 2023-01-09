24f0061c234574f9a29fb5f9bb2268d829919d6b
Geekid812 <geekid812@gmail.com>
2023-01-09 10:48:23 +0100

    Add colorblind firendly team colors

--- src/config.rs
+++ src/config.rs
@@ -12,8 +12,14 @@ pub const MAP_QUEUE_SIZE: usize = 0;
 pub const TMX_FETCH_TIMEOUT: Duration = Duration::from_secs(20);
 pub const TMX_USERAGENT: &'static str = env!("TMX_USERAGENT");
 
-pub const TEAMS: [(&'static str, &'static str); 3] =
-    [("Red", "FF0000"), ("Green", "00FF00"), ("Blue", "0000FF")];
+pub const TEAMS: [(&'static str, &'static str); 6] = [
+    ("Red", "D84315"),
+    ("Green", "8BC34A"),
+    ("Blue", "0095FF"),
+    ("Cyan", "4DD0E1"),
+    ("Pink", "D81B60"),
+    ("Yellow", "FFFF00"),
+];
 
 pub const JOINCODE_LENGTH: u32 = 6;
 pub const JOINCODE_CHARS: [char; 10] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
