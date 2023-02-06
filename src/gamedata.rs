65f710464a72a4a58db3a991c4e4120ea383b81d
Geekid812 <geekid812@gmail.com>
2023-02-06 11:41:19 +0100

    Implement bingo for all directions

--- src/gamedata.rs
+++ src/gamedata.rs
@@ -28,24 +28,11 @@ impl ActiveGameData {
         let mut bingos = Vec::new();
         // Horizontal
         for i in 0..grid_size {
-            let mut line = self.cells[i * grid_size..(i + 1) * grid_size]
+            let line = self.cells[i * grid_size..(i + 1) * grid_size]
                 .iter()
                 .take(grid_size);
-            let first = line
-                .next()
-                .expect("invalid grid_size")
-                .claim
-                .as_ref()
-                .and_then(|c| c.player.team);
-            let unique_team = line.fold(first, |acc, x| {
-                acc.and_then(|y| {
-                    if x.claim.as_ref().and_then(|c| c.player.team) == Some(y) {
-                        Some(y)
-                    } else {
-                        None
-                    }
-                })
-            });
+
+            let unique_team = iter_check_unique_team(line);
 
             if let Some(team) = unique_team {
                 bingos.push(BingoLine {
@@ -56,8 +43,75 @@ impl ActiveGameData {
             }
         }
 
+        // Vertical
+        for i in 0..grid_size {
+            let mut iter = self.cells.iter();
+
+            // Advance by i items to align column
+            for _ in 0..i {
+                iter.next();
+            }
+
+            let col = iter.step_by(grid_size);
+            let unique_team = iter_check_unique_team(col);
+
+            if let Some(team) = unique_team {
+                bingos.push(BingoLine {
+                    direction: Direction::Vertical,
+                    index: i as u32,
+                    team,
+                });
+            }
+        }
+
+        // Diagonal
+        let mut diag0 = Vec::with_capacity(grid_size);
+        let mut diag1 = Vec::with_capacity(grid_size);
+
+        for i in 0..grid_size {
+            diag0.push(self.cells.get(i * grid_size + i).unwrap());
+            diag1.push(self.cells.get((grid_size - 1) * (i + 1)).unwrap());
+        }
+
+        let unique_team0 = iter_check_unique_team(diag0.into_iter());
+        let unique_team1 = iter_check_unique_team(diag1.into_iter());
+
+        if let Some(team) = unique_team0 {
+            bingos.push(BingoLine {
+                direction: Direction::Diagonal,
+                index: 0,
+                team,
+            });
+        }
+
+        if let Some(team) = unique_team1 {
+            bingos.push(BingoLine {
+                direction: Direction::Diagonal,
+                index: 1,
+                team,
+            });
+        }
+
         bingos
     }
+}
+
+fn iter_check_unique_team<'a>(mut iter: impl Iterator<Item = &'a MapCell>) -> Option<usize> {
+    let first = iter
+        .next()
+        .expect("invalid grid_size")
+        .claim
+        .as_ref()
+        .and_then(|c| c.player.team);
+    iter.fold(first, |acc, x| {
+        acc.and_then(|y| {
+            if x.claim.as_ref().and_then(|c| c.player.team) == Some(y) {
+                Some(y)
+            } else {
+                None
+            }
+        })
+    })
 }
 
 fn serialize_time<S: Serializer>(time: &Instant, serializer: S) -> Result<S::Ok, S::Error> {
