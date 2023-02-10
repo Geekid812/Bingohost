33907986f28cdb0c4458055abb0e61637ebe61bc
Geekid812 <geekid812@gmail.com>
2023-02-10 11:24:05 +0100

    Increase maximum packet size

--- src/config.rs
+++ src/config.rs
@@ -63,7 +63,7 @@ pub const TEAMS: [(&'static str, &'static str); 6] = [
 pub const JOINCODE_LENGTH: u32 = 6;
 pub const JOINCODE_CHARS: [char; 10] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-pub const MAXIMUM_PACKET_SIZE: i32 = 512;
+pub const MAXIMUM_PACKET_SIZE: i32 = 2048;
 
 pub mod routes {
     pub mod openplanet {
